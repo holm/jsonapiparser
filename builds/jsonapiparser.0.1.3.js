45dee6994cccf6e51707722445c2e47973bc1d2f
freeall <freeall@gmail.com>
2015-05-28 15:05:03 +0200

    Built 0.1.3

---
+++ builds/jsonapiparser.0.1.3.js
@@ -0,0 +1,56 @@
+(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.jsonapiparser = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+module.exports = function(doc) {
+  doc = JSON.parse(JSON.stringify(doc))
+  var populate = function(elem) {
+    if (!elem.relationships) return elem
+
+    Object.keys(elem.relationships).forEach(function(relationshipName) {
+      var innerPopulate = function(relationship) {
+        var type = relationship.type
+        var id = relationship.id
+        if (!relationships[type]) return relationship;
+
+        relationship.attributes = relationships[type][id].attributes
+        relationship.relationships = relationships[type][id].relationships
+        return relationship
+      }
+
+      if (!Array.isArray(elem.relationships[relationshipName])) {
+        elem.relationships[relationshipName] = innerPopulate(elem.relationships[relationshipName])
+        return
+      }
+
+      elem.relationships[relationshipName] = elem.relationships[relationshipName].map(function(relationship) {
+        return innerPopulate(relationship)
+      })
+    })
+
+    return elem
+  }
+
+  var relationships = {}
+  doc.included.forEach(function(include) {
+    relationships[include.type] = relationships[include.type] || {}
+    relationships[include.type][include.id] = include
+  })
+
+  for (var type in relationships) {
+    for (var id in relationships[type]) {
+      relationships[type][id] = populate(relationships[type][id])
+    }
+  }
+
+  if (doc.data instanceof Array) {
+    doc.data = doc.data.map(function(elem) {
+      return populate(elem)
+    })  
+  } else {
+    doc.data = populate(doc.data)
+  }
+
+
+  return doc
+}
+
+},{}]},{},[1])(1)
+});
