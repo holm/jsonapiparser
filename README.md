656ac84fe21e4340ac73388a1353f5b77d5bbc93
freeall <freeall@gmail.com>
2015-05-28 12:51:22 +0200

    init

---
+++ README.md
@@ -0,0 +1,19 @@
+# jsonapiparser
+
+Try to parse JSONAPI data.
+
+```
+npm install jsonapi-parser
+```
+
+## Usage
+
+``` js
+var parser = require('jsonapiparser')
+
+console.log(parser(yourJsonapiDoc))
+```
+
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,47 @@
+module.exports = function(doc) {
+  doc = JSON.parse(JSON.stringify(doc))
+  var populate = function(elem) {
+    if (!elem.relationships) return elem
+
+    Object.keys(elem.relationships).forEach(function(relationshipName) {
+      var innerPopulate = function(relationship) {
+        var type = relationship.type
+        var id = relationship.id
+        if (!relationships[type]) return relationship;
+
+        relationship.attributes = relationships[type][id].attributes
+        relationship.relationships = relationships[type][id].relationships
+        return relationship
+      }
+
+      if (!Array.isArray(elem.relationships[relationshipName])) {
+        elem.relationships[relationshipName] = innerPopulate(elem.relationships[relationshipName])
+        return
+      }
+
+      elem.relationships[relationshipName] = elem.relationships[relationshipName].map(function(relationship) {
+        return innerPopulate(relationship)
+      })
+    })
+
+    return elem
+  }
+
+  var relationships = {}
+  doc.included.forEach(function(include) {
+    relationships[include.type] = relationships[include.type] || {}
+    relationships[include.type][include.id] = include
+  })
+
+  for (var type in relationships) {
+    for (var id in relationships[type]) {
+      relationships[type][id] = populate(relationships[type][id])
+    }
+  }
+
+  doc.data = doc.data.map(function(elem) {
+    return populate(elem)
+  })
+
+  return doc
+}
